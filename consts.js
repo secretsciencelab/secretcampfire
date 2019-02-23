874d61f441f105355e0b7beee05537df2ed560cc
Aaron <aaron@secretsciencelab.com>
2019-02-22 21:47:49 -0800

    x heroku

--- consts.js
+++ consts.js
@@ -3,7 +3,7 @@
   module.exports.SITE_EMAIL = "kalona@secretcampfire.com";
   module.exports.MASTER_URL = "http://secretcampfire.com";
   module.exports.MASTER_DOMAIN = "secretcampfire.com";
-  module.exports.MASTER_FEED = "https://secretcampfire.herokuapp.com/feed";
-  module.exports.MASTER_NEWS = "https://secretcampfire.herokuapp.com/news.json";
+  module.exports.MASTER_FEED = "https://scampy.secretcampfire.com/feed";
+  module.exports.MASTER_NEWS = "https://scampy.secretcampfire.com/news.json";
   module.exports.NUM_POSTS_PER_FETCH = 5;
 }());
