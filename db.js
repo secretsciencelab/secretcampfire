460afa846ccdd06d1339c4dd00948166930ce7a0
Aaron <aaron@secretsciencelab.com>
2019-03-23 00:52:25 -0700

    typo

--- db.js
+++ db.js
@@ -285,7 +285,7 @@
 
     module.exports.getHotTags = function(cb) {
       _Model('Post').aggregate([
-        { "$match": { "queue": { "$ne": true } } },
+        { "$match": { "queued": { "$ne": true } } },
         { "$unwind": "$tags" },
         { "$group": { "_id": "$tags", "count": {"$sum":1} } },
         { "$match": { "_id": { "$ne": "" } } },
